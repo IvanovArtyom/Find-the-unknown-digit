1b7ca848d81e9a7f6aff74f7925f32de9e23e67b
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-08-21 10:29:32 +0300

    Добавьте файлы проекта.

---
+++ Find the unknown digit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+public class Runes
+{
+    public static void Main()
+    {
+        // Test
+        var t = solveExpression("?*11=??");
+        // ...should return 2
+    }
+
+    public static int solveExpression(string expression)
+    {
+        for (int i = 0; i <= 9; i++)
+        {
+            if (expression.Contains(i.ToString()))
+                continue;
+
+            string[] parts = expression.Replace('?', (char)(i + 48)).Split('=');
+
+            if (new DataTable().Compute(parts[0], "").ToString() == parts[1])
+                return i;
+        }
+
+        return -1;
+    }
+}
